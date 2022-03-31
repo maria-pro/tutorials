f91f45f2e0085445eba462afca224e60e495fa5b
Maria Prokofieva <maria.prokofieva@gmail.com>
2022-03-31 11:48:06 +1100

    hours, dayofweek, months, years, part of the day

--- twitter_dates.R
+++ twitter_dates.R
@@ -5,16 +5,43 @@ library(lubridate)
 
 tweets-read_csv("depression.csv")
 
-tweets<-tweets%>%
+#----------
+#adding days of the week
+
+tweets_dates<-tweets%>%
   mutate(timestamp=lubridate::ymd_hms(created_at),
          day_of_week=lubridate::wday(timestamp, label=TRUE),
-         day_weekday=(lubridate::wday(timestamp) %in% 2:6))
+         day_weekday=(lubridate::wday(timestamp) %in% 2:6),
+         year=year(created_at),
+         month=month(created_at),
+         hours=hour(created_at),
+         part_of_day= factor(hours%/%6, labels=c("night", "morning", "day", "evening")) #roughly! #converting to a factor preserves the order (0 is the smallest in ranking )
+)
 #Monday is very depressing
 
-tweets%>%count(day_of_week, sort=TRUE)
-tweets%>%count(day_weekday, sort=TRUE)
+tweets_dates%>%count(day_of_week, sort=TRUE)
+tweets_dates%>%count(day_weekday, sort=TRUE)
+
+tweets_dates%>% group_by(year)%>%
+  count(day_of_week, sort=TRUE)
+
+#most depressing year,month
+tweets_dates%>%
+  group_by(year, month)%>%
+  summarise(n=n())%>%
+  arrange(desc(n))
 #weekdays are very very depressing or.. may be people do NOT tweet during weekends
 
+#excluding replies to other users
+tweets_dates%>% filter(is.na(in_reply_to_user_id))%>%
+                 ggplot(aes(day_of_week, 
+                            fill=as_factor(year), 
+                            position = "identity"))+
+  geom_bar()
+#adding times 
+
+
+#----------
 #Offsetting dates
 
 #time offset in months
